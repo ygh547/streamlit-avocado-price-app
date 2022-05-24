ec319db31437b124827d52eee1641d0ce4c853ae
ygh547 <ygh547@naver.com>
2022-05-24 11:20:58 +0900

    add

--- app.py
+++ app.py
@@ -10,4 +10,4 @@ if __name__ == '__main__':
 
 # 파이썬 가상환경을 확인한다! 
 # 이 앱은 구글 코랩에서 만든 아보카도 가격 예측 환경과 동일하게 하기 위해서 파이썬 가상환경을 streamlit3.7로 바꿔준다.
-# => vs code 맨 아래 오른쪽에 있는 bace를 streamlit3.7로 변경해준다. 
+# => visual studio code 맨 아래 오른쪽에 있는 bace를 streamlit3.7로 변경해준다. 
